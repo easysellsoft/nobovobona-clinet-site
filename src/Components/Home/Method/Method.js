5e88680cfe7abea72d4a3955f9ea6feb889628e8
easysell <easysellsoft@gmail.com>
2022-07-25 22:05:16 +0600

    new and method component added

--- src/Components/Home/Method/Method.js
+++ src/Components/Home/Method/Method.js
@@ -1,11 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import MethodTable from "./MethodTable";
 // import AddMethod from "./MethodTable";
+import { Box, Button } from "@mui/material";
+import ButtonComp from "./../../Shared/Button/Button";
+import AddPaymentMethod from "./AddPaymentMethod";
 const Method = () => {
+  const [showPayment, setShowPayment] = useState(" ");
+  const [showTable, setShowTable] = useState("click");
+  console.log(showTable);
   return (
     <div>
-      <div style={{ width: "100%" }} className="mb-5 mt-2">
-        <MethodTable/>
+      <div style={{ width: "100%" }} className="mb-5">
+        <Box
+          sx={{
+            mt: 0,
+            mb: 3,
+          }}
+        >
+          <Box
+            sx={[
+              {
+                display: "flex",
+                justifyContent: "flex-start",
+                alignItems: "start",
+                // mt: 2,
+                flexWrap: "wrap",
+              },
+            ]}
+          >
+            <div
+              className="btn_prb"
+              onClick={() => {
+                setShowTable("click");
+
+                setShowPayment("xx");
+              }}
+            >
+              <Box m={2} pt={3}>
+                <Button variant="contained" >
+                  Method
+                </Button>
+              </Box>
+            </div>
+            <div
+              className="btn_prb"
+              onClick={() => {
+                setShowPayment("showPayment");
+                setShowTable("dd");
+              }}
+            >
+              <Box m={2} pt={3}>
+                <Button variant="contained" margin={5}>
+                  New
+                </Button>
+              </Box>
+            </div>
+          </Box>
+        </Box>
+        {showTable === "click" && <MethodTable />}
+        {showPayment === "showPayment" && <AddPaymentMethod />}
       </div>
     </div>
   );
